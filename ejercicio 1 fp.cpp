6c82a433d15e25d4fbf3d6d29f5899afe5b66535
dany-muriel-herencia <drmurielh@unjbg.edu,pe>
2024-06-24 08:22:14 -0500

    es el programa de la cuenta de banco

---
+++ ejercicio 1 fp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int op;
+    cout<<"cajero automatico"<<endl;
+    cout<<"1 . depositar"<<endl;
+    cout<<"2. Retirar"<<endl;
+    cout<<"3. Mostrar saldo"<<endl;
+    cout<<"elija una pcion";cin>>op;
+    switch(op){
+        case 0:
+        cout<<"saliendio......"<<endl;
+        system("pause");
+        break;
+    }
+    return 0;
+}
