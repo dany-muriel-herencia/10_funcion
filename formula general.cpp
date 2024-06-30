57279f2481d4254a9e4114383c650f68189925d6
dany-muriel-herencia <drmurielh@unjbg.edu.pe>
2024-06-30 14:42:30 -0500

    las variables funciona

---
+++ formula general.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void formula_general(int a,int b,int c);
+
+int main(){
+    int a,b,c;
+    cout<<"dame el valor de la primera constante: ";cin>>a;  
+    cout<<"dame el valor de la primera constante: ";cin>>b;
+    cout<<"dame el valor de la primera constante: ";cin>>c;
+    formula_general(a,b,c);
+
+    return 0;
+}
+void formula_general(int a,int b,int c){
+    
+}
+
+
+
